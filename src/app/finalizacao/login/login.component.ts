c64eb77f1e8a2cabc5d89acfc69baad0d4a02cc4
Andrews <andrewsviana28@gmail.com>
2024-08-27 01:07:47 -0300

    Projeto 70%

--- src/app/finalizacao/login/login.component.ts
+++ src/app/finalizacao/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
 
 }
